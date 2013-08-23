b74b7caa3208664b74f19b351f588a1f0fb11447
Jai Ram Rideout <jai.rideout@gmail.com>
2013-08-23 10:57:07 -0400

    Added input fp check and golay_12 check

--- scripts/split_libraries_lea_seq.py
+++ scripts/split_libraries_lea_seq.py
@@ -10,7 +10,7 @@ __maintainer__ = "Jai Ram Rideout"
 __email__ = "jai.rideout@gmail.com"
 
 from qcli import parse_command_line_parameters, make_option
-from qiime.golay import decode_golay_12
+from qiime.golay import decode_golay_12, get_invalid_golay_barcodes
 from qiime.split_libraries import check_map
 from qiime.util import create_dir
 
@@ -62,6 +62,14 @@ def main():
         option_parser.error("Invalid barcode length: %d. Must be greater "
                             "than zero." % barcode_len)
 
+    seq_fps = opts.sequence_read_fps
+
+    if len(seq_fps) != 2:
+        option_parser.error("You must provide exactly two sequence read "
+                            "filepaths, the first for forward reads and "
+                            "second for reverse reads. You specified %d "
+                            "filepaths." % len(seq_fps))
+
     create_dir(opts.output_dir)
 
     with open(opts.mapping_fp, 'U') as map_f:
@@ -77,6 +85,23 @@ def main():
         option_parser.error("Barcodes in mapping file are of length %d, but "
                             "expected barcodes of length %d." %
                             (barcode_len_in_map, barcode_len))
+
+    if barcode_type == 'golay_12':
+        invalid_golay_barcodes = get_invalid_golay_barcodes(bc_to_sid.keys())
+
+        if invalid_golay_barcodes:
+            option_parser.error("Some or all barcodes in the mapping file are "
+                                "not valid golay codes. Do they need to be "
+                                "reverse complemented? If these are not golay "
+                                "barcodes pass --barcode_type 12 to disable "
+                                "barcode error correction, or pass "
+                                "--barcode_type # if the barcodes are not 12 "
+                                "base pairs, where # is the size of the "
+                                "barcodes.\n\nInvalid barcodes: %s" %
+                                ' '.join(invalid_golay_barcodes))
+        
+
+    #for seq_id
 
 
 if __name__ == "__main__":
