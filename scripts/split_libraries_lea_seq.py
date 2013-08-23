e8d95c71ffde44d41cfb3d3500e222f49019c4cb
Jai Ram Rideout <jai.rideout@gmail.com>
2013-08-23 11:54:06 -0400

    Add fastq parsing and barcode extraction

--- scripts/split_libraries_lea_seq.py
+++ scripts/split_libraries_lea_seq.py
@@ -9,9 +9,12 @@ __version__ = "1.7.0-dev"
 __maintainer__ = "Jai Ram Rideout"
 __email__ = "jai.rideout@gmail.com"
 
+from itertools import izip
 from qcli import parse_command_line_parameters, make_option
 from qiime.golay import decode_golay_12, get_invalid_golay_barcodes
+from qiime.parse import MinimalFastqParser
 from qiime.split_libraries import check_map
+from qiime.split_libraries_fastq import correct_barcode, FastqParseError
 from qiime.util import create_dir
 
 script_info = {}
@@ -35,7 +38,11 @@ script_info['optional_options'] = [
                      '6 for length 6 barcodes, or golay_12 for golay error-'
                      'correcting barcodes. Error correction will only be '
                      'applied for golay_12 barcodes [default: %default]',
-                default='golay_12')
+                default='golay_12'),
+    make_option('--max_barcode_errors', type='float',
+                help='the maximum allowable number of errors in the barcode '
+                     'if passing --barcode_type golay_12 [default: %default]',
+                default=1.5),
 ]
 script_info['version'] = __version__
 
@@ -43,6 +50,12 @@ def main():
     option_parser, opts, args = parse_command_line_parameters(**script_info)
 
     barcode_type = opts.barcode_type
+    max_barcode_errors = opts.max_barcode_errors
+
+    if max_barcode_errors < 0:
+        option_parser.error("--max_barcode_errors must be greater than or "
+                            "equal to zero. You provided %.4f." %
+                            max_barcode_errors)
 
     if barcode_type == 'golay_12':
         barcode_correction_fn = decode_golay_12
@@ -99,9 +112,49 @@ def main():
                                 "base pairs, where # is the size of the "
                                 "barcodes.\n\nInvalid barcodes: %s" %
                                 ' '.join(invalid_golay_barcodes))
-        
 
-    #for seq_id
+    header_idx = 0
+    seq_idx = 1
+    qual_idx = 2
+    fwd_read_f = open(seq_fps[0], 'U')
+    rev_read_f = open(seq_fps[1], 'U')
+
+    count_barcode_errors_exceed_max = 0
+
+    for fwd_read, rev_read in izip(
+            MinimalFastqParser(fwd_read_f, strict=False),
+            MinimalFastqParser(rev_read_f, strict=False)):
+        # Confirm match between read headers.
+        if fwd_read[header_idx] != rev_read[header_idx]:
+            raise PairedEndParseError("Headers of forward and reverse reads "
+                                      "do not match. Confirm that the forward "
+                                      "and reverse read fastq files that you "
+                                      "provided have headers that match one "
+                                      "another.")
+        else:
+            header = fwd_read[header_idx]
+
+        # Grab the barcode sequence. It is always at the very end of the
+        # forward read.
+        barcode = fwd_read[seq_idx][-barcode_len:]
+
+        # Correct the barcode (if applicable) and map to sample ID.
+        num_barcode_errors, corrected_barcode, _, sample_id = correct_barcode(
+                barcode, bc_to_sid, barcode_correction_fn)
+
+        # Skip barcodes with too many errors.
+        if num_barcode_errors > max_barcode_errors:
+          count_barcode_errors_exceed_max += 1
+          continue
+
+    fwd_read_f.close()
+    rev_read_f.close()
+
+    print count_barcode_errors_exceed_max
+
+
+class PairedEndParseError(FastqParseError):
+    pass
 
 
 if __name__ == "__main__":
